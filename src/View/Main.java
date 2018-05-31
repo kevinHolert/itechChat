70348e416a8e555cb2ba9fd67e411a34cef1022d
Kevin Holert <kevin.holert@dermalog.com>
2018-05-31 14:06:59 +0200

    initial commit/ added layout of login

---
+++ src/View/Main.java
@@ -0,0 +1,27 @@
+package View;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+@Override
+public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("itechChatLoginWindow.fxml"));
+        primaryStage.setFullScreen(false);
+        primaryStage.setTitle("itech Chat");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.setMinHeight(400);
+        primaryStage.setMinWidth(420);
+        primaryStage.setResizable(false);
+        primaryStage.show();
+        }
+
+public static void main(String[] args) {
+        launch(args);
+        }
+
+}
---
+++ src/View/itechChatLoginWindowController.java
@@ -0,0 +1,54 @@
+package View;
+
+
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class itechChatLoginWindowController {
+
+    @FXML
+    private TextField userNameTextField;
+
+    @FXML
+    private PasswordField passwortBox;
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    void login(ActionEvent event) throws IOException {
+
+        userNameTextField.clear();
+        passwortBox.clear();
+        openMainWindow(event);
+
+    }
+
+    void openMainWindow(ActionEvent event) throws IOException{
+        Parent root = FXMLLoader.load(getClass().getResource("mainWindow.fxml"));
+        Stage stage = new Stage();
+        stage.setTitle("scholzi");
+        stage.setScene(new Scene(root, 450, 450));
+        stage.show();
+        // Hide this current window (if this is what you want)
+        ((Node)(event.getSource())).getScene().getWindow().hide();
+    }
+
+}
+
+
+
+
+
+
